ab665b8611325398ea15e51de109f658ff8ad846
moss <sunruh@proton.me>
2026-07-08 12:50:36 +0200

    feat/add camera controller

---
+++ Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera camera;
+    private Transform cam;
+    private Vector3 cam_pos;
+    private Vector3 cam_rot;
+    private Vector3 cam_scale;
+    private Vector3 cam_offset;
+
+    private float camSize;
+
+    [SerializeField] private float cam_pan_speed;
+    [SerializeField] private float cam_zoom_speed;
+
+    private void OnEnable()
+    {
+        camera = Camera.main;
+        cam = this.transform;
+        cam_pos = cam.position;
+    }
+
+    public void Pan(Vector2 dir)
+    {
+        Vector3 movement = Vector3.ClampMagnitude(dir, 1f);
+        transform.position += Time.deltaTime * cam_pan_speed * movement;
+    }
+
+    public void Zoom(float zoom)
+    {
+        if (zoom > 0)
+        {
+            camera.orthographicSize -= Time.deltaTime * cam_zoom_speed * 10;
+        }
+        else
+        {
+            camera.orthographicSize += Time.deltaTime * cam_zoom_speed * 10;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/InputHandler.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private CameraController cam;
+
+    private InputAction panAction;
+    private InputAction zoomAction;
+
+    private void Start()
+    {
+        panAction = InputSystem.actions.FindAction("Pan");
+        zoomAction = InputSystem.actions.FindAction("Zoom");
+    }
+
+    private void Update()
+    {
+        Vector2 panValue =  panAction.ReadValue<Vector2>();
+        if (panValue.x != 0 || panValue.y != 0)
+        {
+            cam.Pan(panValue);
+        }
+        
+        float zoomValue =  zoomAction.ReadValue<float>();
+        if (zoomValue != 0)
+        {
+            cam.Zoom(zoomValue);
+        }
+    }
+}
