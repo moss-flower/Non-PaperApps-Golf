2c90bbe235bc902dfefdf8629524457e9948c351
moss <sunruh@proton.me>
2026-07-14 12:47:42 +0200

    feat/add overlay tracking to menu manager

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
@@ -6,7 +7,10 @@ public class MenuManager : MonoBehaviour
     //TODO: add "overlay" hierarchy list that allows for displaying several UI components over one another.
     
     private GameObject activeMenu;
+    private List<GameObject> overlays = new List<GameObject>();
     [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject displayRoot;
+    private GameObject activeOverlay;
     void Awake()
     {
         Open(mainMenu);
@@ -20,6 +24,24 @@ public class MenuManager : MonoBehaviour
         }
         activeMenu = menu;
         activeMenu.SetActive(true);
+    }
+
+    public void AddOverlay(GameObject overlay)
+    {
+        overlays.Add(overlay);
+        overlay.transform.SetParent(displayRoot.transform);
+        overlay.transform.SetAsLastSibling();
+        overlay.SetActive(true);
+    }
+
+    public void CloseOverlay()
+    {
+        if (overlays.Count > 0)
+        {
+            GameObject overlay = overlays[^1];
+            overlays.Remove(overlay);
+            overlay.SetActive(false);
+        }
     }
 
     public void Close()
