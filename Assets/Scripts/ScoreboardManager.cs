bd3f3cf8eaddaf5364a510ec5ef240c1c6e80e5d
moss <sunruh@proton.me>
2026-07-17 11:58:53 +0200

    feat/wire mulligan tracking to scene game objects

--- Assets/Scripts/ScoreboardManager.cs
+++ Assets/Scripts/ScoreboardManager.cs
@@ -6,13 +6,20 @@ using UnityEngine;
 public class ScoreboardManager : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
+    [SerializeField] private LevelManager levelManager;
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI rollText;
+    [SerializeField] private TextMeshProUGUI mulliganText;
+    [SerializeField] private TextMeshProUGUI parText;
+    [SerializeField] private TextMeshProUGUI levelName;
 
     private void OnEnable()
     {
         gameManager.OnScoreChanged += UpdateScoreUI;
         gameManager.OnRoll += UpdateRollUI;
+        UpdateParText(levelManager.GetCurrentLevel().par);
+        Initialize();
+        
     }
 
     private void OnDisable()
@@ -21,14 +28,27 @@ public class ScoreboardManager : MonoBehaviour
         gameManager.OnRoll -= UpdateRollUI;
     }
 
+    private void Initialize()
+    {
+        rollText.text = "Roll: ";
+        mulliganText.text = "Mulligan:  3/3";
+        levelName.text = levelManager.GetCurrentLevel().name;
+    }
+
     private void UpdateScoreUI(int score)
     {
         scoreText.text = "Shots: " + score.ToString();
         
     }
 
-    private void UpdateRollUI(int roll)
+    private void UpdateRollUI(int roll, int remainingMulligans)
     {
         rollText.text = "Roll: " + roll.ToString();
+        mulliganText.text = "Mulligan:  " + remainingMulligans.ToString() + "/3";
+    }
+
+    private void UpdateParText(int par)
+    {
+        parText.text = "Par: " + par.ToString();
     }
 }
