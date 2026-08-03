1a869f8be1d25b711917de5ad5b240c491659149
moss <sunruh@proton.me>
2026-08-03 13:20:01 +0200

    feat/add responsiveness to targetable tiles

--- Assets/Scripts/Helpers/TileJiggler.cs
+++ Assets/Scripts/Helpers/TileJiggler.cs
@@ -9,28 +9,33 @@ namespace Helpers
     {
         private Vector3 _startScale;
         private bool _isHovered;
-        private void Awake()
+        private bool canAnimate;
+        
+        [SerializeField] private float strength;
+
+        public void Initialize(bool canAnimate)
         {
             _startScale = transform.localScale;
+            this.canAnimate = canAnimate;
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            if (_isHovered){return;}
+            if (_isHovered || !canAnimate){return;}
+            
+            var scale = _startScale * strength;
             _isHovered = true;
             var transform = this.transform;
-            Tween.Scale(transform, new Vector3(1.1f, 1.1f, 1.1f), 0.2f);
-            Debug.unityLogger.Log("OnPointerEnter", transform);
+            Tween.Scale(transform, scale, 0.2f);
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            if (!_isHovered){return;}
+            if (!_isHovered || !canAnimate){return;}
             _isHovered = false;
             var transform = this.transform;
             //
             Tween.Scale(transform, _startScale, 0.2f);
-            Debug.unityLogger.Log("OnPointerExit", transform);
         }
     }
 }
